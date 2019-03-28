827a29e31d8e85d10cbfefc7d2f0d995003f4d95
PiniponSelvagem <piniponselvagem@gmail.com>
2019-03-28 23:48:31 +0000

    Added queries.

--- src/main/java/com/piniponselvagem/azerolib/utils/queries/lazy/LazyQueries.java
+++ src/main/java/com/piniponselvagem/azerolib/utils/queries/lazy/LazyQueries.java
@@ -1,5 +1,43 @@
 package com.piniponselvagem.azerolib.utils.queries.lazy;
 
-public class LazyQueries {
+import com.piniponselvagem.azerolib.utils.queries.BaseQueries;
+import com.piniponselvagem.azerolib.utils.queries.Queries;
+import com.piniponselvagem.azerolib.utils.queries.lazy.iterators.FilterIterator;
+import com.piniponselvagem.azerolib.utils.queries.lazy.iterators.LimitIterator;
+import com.piniponselvagem.azerolib.utils.queries.lazy.iterators.MapIterator;
 
+import java.util.Iterator;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class LazyQueries<T> extends BaseQueries<T> {
+
+    private LazyQueries(Iterable<T> src) {
+        super(src);
+    }
+
+    @Override
+    public Queries<T> filter(Predicate<T> filter) {
+        return new LazyQueries<>(new Iterable<T>() {
+            //Kept has an example without using lambda
+            @Override
+            public Iterator<T> iterator() {
+                return new FilterIterator<>(src.iterator(), filter);
+            }
+        });
+    }
+
+    @Override
+    public <R> Queries<R> map(Function<T, R> mapper) {
+        return new LazyQueries<>(() -> new MapIterator<>(src.iterator(), mapper));
+    }
+
+    @Override
+    public Queries<T> limit(int n) {
+        return new LazyQueries<>(() -> new LimitIterator<>(src.iterator(), n));
+    }
+
+    public static <T> Queries<T> of(Iterable<T> src) {
+        return new LazyQueries<>(src);
+    }
 }
---
+++ src/main/java/com/piniponselvagem/azerolib/utils/queries/lazy/iterators/MapIterator.java
@@ -0,0 +1,25 @@
+package com.piniponselvagem.azerolib.utils.queries.lazy.iterators;
+
+import java.util.Iterator;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class MapIterator<T, R> extends BaseIterator<R> {
+
+    private final Function<T, R> mapper;
+    private final Iterator<T> src;
+
+    public MapIterator(Iterator<T> src, Function<T, R> mapper) {
+        this.src = src;
+        this.mapper = mapper;
+    }
+
+    @Override
+    protected boolean tryAdvance(Consumer<R> consumer) {
+        if(!src.hasNext()) {
+            return false;
+        }
+        consumer.accept(mapper.apply(src.next()));
+        return true;
+    }
+}
